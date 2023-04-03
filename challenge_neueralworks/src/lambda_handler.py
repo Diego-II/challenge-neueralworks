1829ba3b6ae9de926d6e43a6943c7c5c02d7bea6
Diego-II <diego.irarrazaval@ug.uchile.cl>
2023-04-03 02:43:33 -0400

    getting lambda handler ready for production

--- challenge_neueralworks/src/lambda_handler.py
+++ challenge_neueralworks/src/lambda_handler.py
@@ -18,27 +18,33 @@ def lambda_handler(
     # List models in models directory
     models = glob('/models/*')
     logger.info('Models: %s', str(models))
-    # load the dumped pipeline
-    pipeline = get_pipeline()
-    logger.info('Loaded model')
+    if event["resource"] == '/get-prediction':
+        # load the dumped pipeline
+        pipeline = get_pipeline()
+        logger.info('Loaded model')
+        
+        logger.info('Model: %s', str(pipeline.__dict__))
     
-    logger.info('Model: %s', str(pipeline.__dict__))
+        # extract input data from the event
+        input_data = event["body"]["data"]
+        
+        # convert input data to a numpy array
+        # input_array = np.array(list(input_data.values())).reshape(1, -1)
+        
+        input_df = pd.DataFrame([input_data])
+        # generate predictions using the predict_proba method
+        prediction = pipeline.predict_proba(input_df)
+        
+        # return prediction as JSON response
+        response = {
+            'prediction': prediction.tolist()[0]
+        }
+        return {
+            'statusCode': 200,
+            'body': json.dumps(response)
+        }
     
-    # extract input data from the event
-    input_data = event['data']
-    
-    # convert input data to a numpy array
-    # input_array = np.array(list(input_data.values())).reshape(1, -1)
-    
-    input_df = pd.DataFrame([input_data])
-    # generate predictions using the predict_proba method
-    prediction = pipeline.predict_proba(input_df)
-    
-    # return prediction as JSON response
-    response = {
-        'prediction': prediction.tolist()[0]
-    }
     return {
-        'statusCode': 200,
-        'body': json.dumps(response)
+        'statusCode': 404,
+        'body': json.dumps('Not found')
     }

25d45bc809e1b4dcfa67c094f5e13e75794cf480
Diego-II <diego.irarrazaval@ug.uchile.cl>
2023-04-03 02:43:12 -0400

    first try to fix dev stage

--- infra/api-gateway/inference-endpoint.tf
+++ infra/api-gateway/inference-endpoint.tf
@@ -7,6 +7,7 @@ resource "aws_api_gateway_resource" "get-prediction-resource" {
 resource "aws_api_gateway_method" "get-prediction-method" {
   rest_api_id   = aws_api_gateway_rest_api.api-gateway.id
   resource_id   = aws_api_gateway_resource.get-prediction-resource.id
+  api_key_required = true
   http_method   = "POST"
   authorization = "NONE"
 }
